81749f84fdce46c438dfd3fefb8283e497ad34db
200263830 <bpatel@tph.ca>
2020-02-25 17:06:38 -0500

    change in date format

--- inject.js
+++ inject.js
@@ -6,7 +6,7 @@ if(hour>12){
 }else{
   var numberOfdays = 2;
 }
-document.getElementById("number_of_days").innerHTML = "You will recive delivery on <strong>" + addBusinessDays(d, numberOfdays) +"<strong>";
+document.getElementById("number_of_days").innerHTML = "Delivered by <br/> <strong>" + addBusinessDays(d, numberOfdays) +"<strong>";
 function getHour() {
   var d = new Date();
   var n = d.getHours();
@@ -15,9 +15,11 @@ function getHour() {
 
 
 function addBusinessDays(d,n) {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var month = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
     d = new Date(d.getTime());
     var day = d.getDay();
     d.setDate(d.getDate() + n + (day === 6 ? 2 : +!day) + (Math.floor((n - 1 + (day % 6 || 1)) / 5) * 2));    
-    var myDate = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+    var myDate = days[d.getDay()] + ", " + month[d.getMonth()] + " " + d.getDate();
     return myDate;
 }
